f19e3d7f4dbefdefe6a9d5b76a8c00948ff51e0c
Nicholas Sixbury <nico6bury@ksu.edu>
2020-09-11 13:35:59 -0500

    Added the IOrderItem interface, which has the price, calories, and specialInstructions getters. This interface will be implemented by all entrees, drinks, and sides.

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BleakwindBuffet.Data
+{
+    /// <summary>
+    /// interface for all items that can be ordered, has
+    /// price, calories, and special instructions for the item
+    /// </summary>
+    interface IOrderItem
+    {
+        /// <summary>
+        /// The price of the item in question, in dollars
+        /// </summary>
+        double Price { get; }
+        
+        /// <summary>
+        /// The number of calories the item in question has
+        /// </summary>
+        uint Calories { get; }
+        
+        /// <summary>
+        /// A list of special instructions for the item in question,
+        /// an empty list if there are no special instructions
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+    }//end interface
+}//end namespace
