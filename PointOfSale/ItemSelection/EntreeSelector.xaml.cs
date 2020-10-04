b252763c8817e7cdb454ecb46e0c26a0707f4957
Nicholas Sixbury <nico6bury@ksu.edu>
2020-10-03 22:09:41 -0500

    Finished the code to get the user from EntreeSelector to customizing their item. it was surprisingly easy. I may have underestimated the excellence of ItemCustomizer. Noice!

--- PointOfSale/ItemSelection/EntreeSelector.xaml.cs
+++ PointOfSale/ItemSelection/EntreeSelector.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BleakwindBuffet.Data.Entrees;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -27,6 +28,13 @@ namespace PointOfSale.ItemSelection
         public EntreeSelector()
         {
             InitializeComponent();
+            bbButton.T = typeof(BriarheartBurger);
+            ddButton.T = typeof(DoubleDraugr);
+            ttButton.T = typeof(ThalmorTriple);
+            ssButton.T = typeof(SmokehouseSkeleton);
+            gooButton.T = typeof(GardenOrcOmelette);
+            ppButton.T = typeof(PhillyPoacher);
+            ttbButton.T = typeof(ThugsTBone);
         }//end constructor
 
         /// <summary>
@@ -37,5 +45,58 @@ namespace PointOfSale.ItemSelection
         {
             ItemSelector.itemSelector.Child = ItemSelector.ics;
         }//end GoBack
+
+        /// <summary>
+        /// EventHandler for one of the ItemButtons to get its item
+        /// added to ItemCustomizer so that it can be customized.
+        /// </summary>
+        public void GetSelectedItem(object sender, RoutedEventArgs e)
+        {
+            if(sender is ItemButton button)
+            {
+                //go ahead and change screens and stuff i guess
+                dynamic item;
+                switch (button.Content)
+                {
+                    case "Briarheart Burger":
+                        item = new BriarheartBurger();
+                        break;
+                    case "Double Draugr":
+                        item = new DoubleDraugr();
+                        break;
+                    case "Thalmor Triple":
+                        item = new ThalmorTriple();
+                        break;
+                    case "Smokehouse Skeleton":
+                        item = new SmokehouseSkeleton();
+                        break;
+                    case "Garden Orc Omelette":
+                        item = new GardenOrcOmelette();
+                        break;
+                    case "Philly Poacher":
+                        item = new PhillyPoacher();
+                        break;
+                    case "Thugs T-Bone":
+                        item = new ThugsTBone();
+                        break;
+                    default:
+                        throw new NotImplementedException("That button isn't fully " +
+                            "implemented yet. See source of error for more detail.");
+                }//end switch case to determine item type
+
+                //send item to ItemCustomizer
+                SendToCustomizer(button);
+
+                //Switch Screens
+                ItemSelector.itemSelector.Child = ItemSelector.ic;
+            }//end if sender is right type
+        }//end GetSelectedItem event handler
+
+        private void SendToCustomizer(ItemButton sender)
+        {
+            ItemSelector.ic.cameFrom = "Entree";
+            ItemSelector.ic.GetBooleanVars(sender);
+            ItemSelector.ic.PopulateCheckBoxes();
+        }
     }//end partial class
 }//end namespace
