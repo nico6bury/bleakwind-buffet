843e44d31152d2b1ade952f7c0f873635c95d2d7
Nicholas Sixbury <nico6bury@ksu.edu>
2020-11-01 18:54:37 -0600

    Made the index page, which displays the menu items.

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using BleakwindBuffet.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -15,11 +17,13 @@ namespace Website.Pages
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-        }
+        }//end constructor
+
+        
 
         public void OnGet()
         {
 
-        }
-    }
-}
+        }//end OnGet()
+    }//end class
+}//end namespace
