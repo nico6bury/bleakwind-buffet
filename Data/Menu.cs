2bcf71c7f4aebad89e4aef57cf32de6dae664dd7
Nicholas Sixbury <nico6bury@ksu.edu>
2020-11-08 14:16:48 -0600

    Finished the last of the filtering methods and moved them to Menu where they'll probably be more easily testable.

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -5,6 +5,7 @@ using BleakwindBuffet.Data.Sides;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 /*
  * Author: Nicholas Sixbury
@@ -153,6 +154,118 @@ namespace BleakwindBuffet.Data
             return full;
         }//end FullMenu()
 
-        
+        /// <summary>
+        /// Finds all items with a certain string of text present within their ToString().
+        /// </summary>
+        /// <param name="items">The collection of IOrderItems to search through</param>
+        /// <param name="terms">the string you want to search for</param>
+        /// <returns>Returns a filtered list as an IEnumerable</returns>
+        public static IEnumerable<IOrderItem> Search(IEnumerable<IOrderItem> items, string terms)
+        {
+            if (terms == null) return items;
+            else
+            {
+                List<IOrderItem> results = new List<IOrderItem>();
+
+                foreach (IOrderItem item in Menu.FullMenu())
+                {
+                    if (item.ToString().ToLower().Contains(terms.ToLower())) results.Add(item);
+                }//end looping foreach item on the menu
+
+                return results;
+            }//end else we can go ahead and search
+        }//end Search(terms)
+
+        /// <summary>
+        /// Finds all the items that have one of a certain list of order types. The valid types
+        /// are "Entrees", "Sides", and "Drinks".
+        /// </summary>
+        /// <param name="items">The collection of IOrderItems to search through</param>
+        /// <param name="types">the list of types that you want items to have</param>
+        /// <returns>Returns a filtered list as an IEnumerable</returns>
+        public static IEnumerable<IOrderItem> FilterByOrderType(IEnumerable<IOrderItem> items, IEnumerable<string> types)
+        {
+            if (types.Count() == 0) return items;
+            else
+            {
+                List<IOrderItem> results = new List<IOrderItem>();
+                foreach (IOrderItem item in items)
+                {
+                    foreach (string type in types)
+                    {
+                        switch (type)
+                        {
+                            case "Entrees":
+                                if (item is Entree && !results.Contains(item))
+                                    results.Add(item);
+                                break;
+                            case "Sides":
+                                if (item is Side && !results.Contains(item))
+                                    results.Add(item);
+                                break;
+                            case "Drinks":
+                                if (item is Drink && !results.Contains(item))
+                                    results.Add(item);
+                                break;
+                            default:
+                                // How exceptional:
+                                throw new Exception("I'm the exception!");
+                        }//end switch case
+                    }//end looping foreach type
+                }//end looping foreach item
+
+                return results;
+            }//end else
+        }//end FilterByOrderType
+
+        /// <summary>
+        /// Finds all the items in a collection of IOrderItems whose calorie counts are between
+        /// a certain specified range
+        /// </summary>
+        /// <param name="items">the collection of items to filter</param>
+        /// <param name="CalMin">the minimum calorie count</param>
+        /// <param name="CalMax">the maximum calories count</param>
+        /// <returns>Returns a filtered collection of IOrderItems</returns>
+        public static IEnumerable<IOrderItem> FilterByCalories(IEnumerable<IOrderItem> items, int CalMin, int CalMax)
+        {
+            if (CalMin > CalMax) return items;
+            else
+            {
+                List<IOrderItem> results = new List<IOrderItem>();
+
+                foreach (IOrderItem item in Menu.FullMenu())
+                {
+                    if (item.Calories >= CalMin && item.Calories <= CalMax)
+                        results.Add(item);
+                }//end testing each item in menu
+
+                return results;
+            }//end else we need to filter things
+        }//end FilterByCalories(CalMin, CalMax)
+
+        /// <summary>
+        /// Finds all the items in a collection of IOrderItems whose Price is between
+        /// a certain specified range
+        /// </summary>
+        /// <param name="items">the collection to filter</param>
+        /// <param name="PriceMin">the minimum price</param>
+        /// <param name="PriceMax">the maximum price</param>
+        /// <returns>Returns a filtered collection</returns>
+        public static IEnumerable<IOrderItem> FilterByPrice(IEnumerable<IOrderItem> items, double PriceMin, double PriceMax)
+        {
+            if (PriceMin > PriceMax) return items;
+            else
+            {
+                List<IOrderItem> results = new List<IOrderItem>();
+
+                foreach (IOrderItem item in Menu.FullMenu())
+                {
+                    if (item.Price >= PriceMin && item.Price <= PriceMax)
+                        results.Add(item);
+                }//end testing each item in menu
+
+                return results;
+            }//end else we need to filter things
+        }//end FilterByPrice
     }//end class
 }//end namespace
---
+++ Website/Pages/Search.cshtml.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Sides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public static string SearchTerms;
+        public IEnumerable<IOrderItem> items;
+        public static string[] OrderTypes { get; set; }
+        public static int CaloriesMin;
+        public static int CaloriesMax;
+        public static double PriceMin;
+        public static double PriceMax = 8.32;
+        public void OnGet(double PriceMin, double PriceMax)
+        {
+            SearchTerms = Request.Query["SearchTerms"];
+            OrderTypes = Request.Query["OrderTypes"];
+            string CaloriesMinStr = Request.Query["CaloriesMin"];
+            string CaloriesMaxStr = Request.Query["CaloriesMax"];
+            
+            if (CaloriesMinStr == null || CaloriesMinStr.Length == 0) CaloriesMin = 0;
+            else CaloriesMin = Convert.ToInt32(CaloriesMinStr);
+            
+            if (CaloriesMaxStr == null || CaloriesMaxStr.Length == 0) CaloriesMax = 982;
+            else CaloriesMax = Convert.ToInt32(CaloriesMaxStr);
+
+            string PriceMinStr = Request.Query["PriceMin"];
+            string PriceMaxStr = Request.Query["PriceMax"];
+
+            if (PriceMinStr == null || PriceMinStr.Length == 0) PriceMin = 0;
+            else PriceMin = Convert.ToDouble(PriceMinStr);
+
+            if (PriceMaxStr == null || PriceMaxStr.Length == 0) PriceMax = 8.32;
+            else PriceMax = Convert.ToDouble(PriceMaxStr);
+
+            items = Menu.FullMenu();
+
+            items = Menu.Search(items, SearchTerms);
+            items = Menu.FilterByOrderType(items, OrderTypes);
+            items = Menu.FilterByCalories(items, CaloriesMin, CaloriesMax);
+            items = Menu.FilterByPrice(items, PriceMin, PriceMax);
+        }//end OnGet()
+    }//end class
+}//end namespace
